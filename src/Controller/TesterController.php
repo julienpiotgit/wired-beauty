2d24e91643c2294ae31c1d9ad769f0668dfadda3
Pierrick Sandrin <pcksandrin@gmail.com>
2022-06-13 21:25:23 +0200

    ListCampaign

--- src/Controller/TesterController.php
+++ src/Controller/TesterController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Application;
 use App\Entity\Campaign;
+use App\Entity\Session;
 use App\Entity\Status;
+use App\Repository\StatusRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,5 +46,42 @@ class TesterController extends AbstractController
             'allCampaignSoon' => $allCampaignSoon,
             'allCampaignFinish' => $allCampaignFinish
         ]);
+    }
+
+    /**
+     * @Route("/add_application", name="add_application")
+     */
+    public function add_application(UserRepository $userRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $currentUser = $this->getUser();
+        $user = $userRepository->find($currentUser);
+
+        // Is it an Ajax Request ?
+        if (!$request->isXmlHttpRequest())
+            return new JsonResponse(array('status' => 'Error'), 400);
+
+        // Request has request data ?
+        if (!isset($request->request))
+            return new JsonResponse(array('status' => 'Error'), 400);
+
+        $campaignId = $request->request->get("campaign");
+
+        $statusPending = $this->entityManager->getRepository(Status::class)->findOneBy(["name" => "pending"]);
+
+        $session = $this->entityManager->getRepository(Session::class)->findBy(["campaign" => $campaignId]);
+
+        $sessioncount = $this->entityManager->getRepository(Session::class)->count(["campaign" => $campaignId]);
+
+        $sessionRandom = random_int(1, $sessioncount);
+
+        $application = new Application();
+        $application->setUser($user);
+        $application->setStatus($statusPending);
+        $application->setSession($session[$sessionRandom-1]);
+
+        $entityManager->persist($application);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("admin");
     }
 }
